523818370ed9a091582a312f44369f15073c94ee
greegg <ljzxgj8@163.com>
2020-01-31 00:06:43 +0800

    Add files via upload

---
+++ Version1.0/QPSKDecode.m
@@ -0,0 +1,21 @@
+function DQPSKCode = QPSKDecode(QPSKCode, Length)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%该函数实现QPSK解调
+%%%%%QPSKCode为待解调的序列
+%%%%%Length为原始序列的长度
+%%%%%DQPSKCode为解调后的序列
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+DQPSKCode = zeros(size(QPSKCode));
+for i = 1 : Length
+    L = 2*i-1;
+    R = 2*i;
+    if QPSKCode(L)>=0 &  QPSKCode(R)>=0
+        DQPSKCode(L:R) = [1 1];
+    elseif QPSKCode(L)<=0 &  QPSKCode(R)>=0
+        DQPSKCode(L:R) = [0 1];
+    elseif QPSKCode(L)<=0 &  QPSKCode(R)<=0
+        DQPSKCode(L:R) = [0 0];
+    else
+        DQPSKCode(L:R) = [1 0];
+    end
+end
